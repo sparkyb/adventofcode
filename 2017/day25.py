1bedd3df3b9427b01814d6faf32f6b1f8c724b85
Ben Buchwald <ben@ngcreative.com>
2017-12-26 11:02:01 -0500

    actually parse day 25 input

--- 2017/day25.py
+++ 2017/day25.py
@@ -12,27 +12,29 @@ def get_input(filename=None):
     with open(filename) as fp:
         input = fp.read().strip()
 
-    return input.split('\n')
+    lines = input.split('\n')
+    initial_state = re.search(r'^Begin in state (\w+)\.$', lines.pop(0)).group(1)
+    iterations = int(re.search(r'^Perform a diagnostic checksum after (\d+) steps?\.$', lines.pop(0)).group(1))
+    assert not lines.pop(0)
+    states = {}
+    while lines:
+        state = re.search(r'^In state (\w+):$', lines.pop(0)).group(1)
+        while lines and lines[0]:
+            val = int(re.search('^\s*If the current value is ([01]):$', lines.pop(0)).group(1))
+            write = int(re.search('^\s*- Write the value ([01])\.$', lines.pop(0)).group(1))
+            move = re.search('^\s*- Move one slot to the (left|right)\.$', lines.pop(0)).group(1) == 'left' and -1 or 1
+            next = re.search('^\s*- Continue with state (\w+)\.$', lines.pop(0)).group(1)
+            states.setdefault(state, {})[val] = {'write': write, 'move': move, 'next': next}
+        if lines:
+            lines.pop(0)
+    return initial_state, iterations, states
 # end get_input
 
 def part1(input):
-    states = {}
-    states['A'] = [{'write':1,'move':1,'next':'B'},
-                   {'write':0,'move':-1,'next':'B'}]
-    states['B'] = [{'write':0,'move':1,'next':'C'},
-                   {'write':1,'move':-1,'next':'B'}]
-    states['C'] = [{'write':1,'move':1,'next':'D'},
-                   {'write':0,'move':-1,'next':'A'}]
-    states['D'] = [{'write':1,'move':-1,'next':'E'},
-                   {'write':1,'move':-1,'next':'F'}]
-    states['E'] = [{'write':1,'move':-1,'next':'A'},
-                   {'write':0,'move':-1,'next':'D'}]
-    states['F'] = [{'write':1,'move':1,'next':'A'},
-                   {'write':1,'move':-1,'next':'E'}]
-    state = 'A'
+    state, iterations, states = input
     cursor = 0
     tape = defaultdict(int)
-    for i in xrange(12629077):
+    for i in xrange(iterations):
         s = states[state][tape[cursor]]
         tape[cursor] = s['write']
         cursor += s['move']
