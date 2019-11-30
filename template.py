54daaaba82bfa8c876f6f36ffb717d4b37443a31
Ben Buchwald <sparkyb@gmail.com>
2019-11-30 16:24:26 -0500

    converted template to python 3

--- template.py
+++ template.py
@@ -1,16 +1,17 @@
-import os.path
-import re
-import math
 from collections import defaultdict
 import itertools
-import md5
+import math
+from hashlib import md5
 import msvcrt
+import numpy as np
+import os.path
+import re
 import sys
 
 
 def get_input(filename=None):
   if not filename:
-    filename = os.path.splitext(os.path.basename(__file__))[0]+'.txt'
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
   with open(filename) as fp:
     input = fp.read().rstrip()
 
@@ -25,9 +26,10 @@ def part2(input):
 
 
 if __name__ == '__main__':
-  from optparse import OptionParser
-  parser = OptionParser(usage='%prog [options] [<input.txt>]')
-  options, args = parser.parse_args()
-  input = get_input(*args)
-  print part1(input)
-  print part2(input)
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('input', nargs='?', metavar='input.txt')
+  args = parser.parse_args()
+  input = get_input(args.input)
+  print(part1(input))
+  print(part2(input))
