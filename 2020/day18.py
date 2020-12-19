042beb8a544542663b8f4b888a2194a21bd130f2
Ben Buchwald <bbuchwald@boxfort.com>
2020-12-19 01:02:36 -0500

    day18 can handle order of operations without adding parens

--- 2020/day18.py
+++ 2020/day18.py
@@ -13,28 +13,26 @@ import sys
 #import numpy as np
 
 
-def get_input(filename=None):
-  if not filename:
-    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
-  with open(filename) as fp:
-    input = fp.read().rstrip('\n')
-
-  return input.split('\n')
-
-
 OPS = {
     '+': operator.add,
     '*': operator.mul,
 }
 
 
-def tokenize(line):
-  return [int(token) if token.isdigit() else token
+def get_input(filename=None):
+  if not filename:
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+  with open(filename) as fp:
+    input = fp.read().rstrip('\n')
+
+  return [[int(token) if token.isdigit() else token
            for token in re.findall(r'\d+|[(){}]'.format(''.join(OPS.keys())),
                                    line)]
+          for line in input.split('\n')]
 
 
-def eval_math(tokens, i=0):
+def eval_math(tokens, i=0, do_last=None):
+  results = []
   left = 0
   op = operator.add
   while i < len(tokens):
@@ -42,27 +40,32 @@ def eval_math(tokens, i=0):
       op = OPS[tokens[i]]
     else:
       if tokens[i] == '(':
-        right, i = eval_math(tokens, i + 1)
+        right, i = eval_math(tokens, i + 1, do_last=do_last)
       elif tokens[i] == ')':
+        if do_last:
+          results.append(left)
+          left = functools.reduce(OPS[do_last], results)
         return left, i
       else:
         right = tokens[i]
-      left = op(left, right)
+      if not do_last or op != OPS[do_last]:
+        left = op(left, right)
+      else:
+        results.append(left)
+        left = right
     i += 1
+  if do_last:
+    results.append(left)
+    left = functools.reduce(OPS[do_last], results)
   return left
 
 
 def part1(input):
-  return sum(eval_math(tokenize(line)) for line in input)
+  return sum(eval_math(line) for line in input)
 
 
 def part2(input):
-  return sum(
-      eval_math(tokenize(
-          '(' +
-          line.replace('(', '((').replace(')', '))').replace('*', ') * (') +
-          ')'))
-      for line in input)
+  return sum(eval_math(line, do_last='*') for line in input)
 
 
 if __name__ == '__main__':
