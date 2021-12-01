ee6672cc8621436e44122ae9d46367cdd65aff99
Ben Buchwald <bbuchwald@boxfort.com>
2021-12-01 00:12:49 -0500

    2021 day 1

---
+++ 2021/day1.py
@@ -0,0 +1,48 @@
+import collections
+from collections import defaultdict
+import enum
+import functools
+import itertools
+import math
+import msvcrt
+import operator
+import os.path
+import re
+import sys
+
+#import numpy as np
+
+
+def get_input(filename=None):
+  if not filename:
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+  with open(filename) as fp:
+    input = fp.read().rstrip('\n')
+
+  return list(map(int, input.split('\n')))
+
+
+def part1(input):
+  count = 0
+  for a, b in zip(input, input[1:]):
+    if b > a:
+      count += 1
+  return count
+
+
+def part2(input):
+  count = 0
+  for a, b in zip(input, input[3:]):
+    if b > a:
+      count += 1
+  return count
+
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('input', nargs='?', metavar='input.txt')
+  args = parser.parse_args()
+  input = get_input(args.input)
+  print(part1(input))
+  print(part2(input))
