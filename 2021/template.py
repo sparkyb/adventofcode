ecb385305cbe9edea54951dacede062728907bdf
Ben Buchwald <bbuchwald@boxfort.com>
2021-12-01 00:12:41 -0500

    2021 template

---
+++ 2021/template.py
@@ -0,0 +1,40 @@
+import collections
+from collections import defaultdict
+import enum
+import functools
+import itertools
+import math
+import msvcrt
+import operator
+import os.path
+import re
+import sys
+
+#import numpy as np
+
+
+def get_input(filename=None):
+  if not filename:
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+  with open(filename) as fp:
+    input = fp.read().rstrip('\n')
+
+  return input.split('\n')
+
+
+def part1(input):
+  return None
+
+
+def part2(input):
+  return None
+
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('input', nargs='?', metavar='input.txt')
+  args = parser.parse_args()
+  input = get_input(args.input)
+  print(part1(input))
+  print(part2(input))
