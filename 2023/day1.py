0c8fd6f415482fbcc1a110670de4ba35a6496895
Ben Buchwald <sparkyb@gmail.com>
2023-12-01 00:21:02 -0500

    2023 day 1

---
+++ 2023/day1.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import collections
+from collections import defaultdict
+import enum
+import functools
+import itertools
+import math
+import msvcrt
+import operator
+import os.path
+import re
+import sys
+
+#import numpy as np
+
+
+def get_input(filename=None):
+  if not filename:
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+  with open(filename) as fp:
+    input = fp.read().rstrip('\n')
+
+  return input.split('\n')
+
+
+def part1(input):
+  total = 0
+  for line in input:
+    digits = [int(c) for c in line if c.isdigit()]
+    total += digits[0] * 10 + digits[-1]
+  return total
+
+
+NUMBER_WORDS = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+  'zero': 0,
+}
+
+
+def part2(input):
+  total = 0
+  for line in input:
+    digits = []
+    for i, c in enumerate(line):
+      if c.isdigit():
+        digits.append(int(c))
+      else:
+        for word, number in NUMBER_WORDS.items():
+          if line[i:i + len(word)] == word:
+            digits.append(number)
+    total += digits[0] * 10 + digits[-1]
+  return total
+
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('-c', '--clip', '--copy', action='store_true',
+                      help='Copy answer to clipboard')
+  parser.add_argument('-p', '--part', type=int, choices=(1, 2),
+                      help='Which part to run (default: both)')
+  parser.add_argument('-1', '--part1', action='store_const', dest='part',
+                      const=1, help='Part 1 only')
+  parser.add_argument('-2', '--part2', action='store_const', dest='part',
+                      const=2, help='Part 2 only')
+  parser.add_argument('input', nargs='?', metavar='input.txt')
+  args = parser.parse_args()
+  if args.clip:
+    import pyperclip
+  input = get_input(args.input)
+  if not args.part or args.part == 1:
+    answer1 = part1(input)
+    print(answer1)
+    if args.clip and answer1 is not None:
+      pyperclip.copy(str(answer1))
+  if not args.part or args.part == 2:
+    answer2 = part2(input)
+    print(answer2)
+    if args.clip and answer2 is not None:
+      pyperclip.copy(str(answer2))
