074aca990cf4b393f3c380b4fc6c8bb27d13a351
Ben Buchwald <sparkyb@gmail.com>
2024-12-01 17:53:58 -0500

    2024 day 1

---
+++ 2024/day1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import collections
+from collections import defaultdict
+import enum
+import functools
+import itertools
+import math
+import msvcrt
+import operator
+import os.path
+import re
+import sys
+
+#import numpy as np
+
+
+def get_input(filename=None):
+  if not filename:
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+  with open(filename) as fp:
+    input = fp.read().rstrip('\n')
+
+  return list(zip(*[[int(n) for n in line.split()]
+                    for line in input.split('\n')]))
+
+
+def part1(input):
+  left, right = input
+  left = sorted(left)
+  right = sorted(right)
+  return sum(abs(l - r) for l, r in zip(left, right))
+
+
+def part2(input):
+  left, right = input
+  return sum(l * right.count(l) for l in left)
+
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('-c', '--clip', '--copy', action='store_true',
+                      help='Copy answer to clipboard')
+  parser.add_argument('-p', '--part', type=int, choices=(1, 2),
+                      help='Which part to run (default: both)')
+  parser.add_argument('-1', '--part1', action='store_const', dest='part',
+                      const=1, help='Part 1 only')
+  parser.add_argument('-2', '--part2', action='store_const', dest='part',
+                      const=2, help='Part 2 only')
+  parser.add_argument('input', nargs='?', metavar='input.txt')
+  args = parser.parse_args()
+  if args.clip:
+    import pyperclip
+  input = get_input(args.input)
+  if not args.part or args.part == 1:
+    answer1 = part1(input)
+    print(answer1)
+    if args.clip and answer1 is not None:
+      pyperclip.copy(str(answer1))
+  if not args.part or args.part == 2:
+    answer2 = part2(input)
+    print(answer2)
+    if args.clip and answer2 is not None:
+      pyperclip.copy(str(answer2))
