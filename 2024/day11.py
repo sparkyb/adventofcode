af3ce4e1efe959ae371657aa5c30813311dc4b5e
Ben Buchwald <sparkyb@gmail.com>
2024-12-11 00:14:48 -0500

    2024 day 11

---
+++ 2024/day11.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import collections
+from collections import defaultdict
+import enum
+import functools
+import itertools
+import math
+import msvcrt
+import operator
+import os.path
+import re
+import sys
+
+#import numpy as np
+
+
+def get_input(filename=None):
+  if not filename:
+    filename = os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+  with open(filename) as fp:
+    input = fp.read().rstrip('\n')
+
+  return [int(stone) for stone in input.split()]
+
+
+@functools.cache
+def blink(stone, n=1):
+    if stone == 0:
+      next_stones = [1]
+    else:
+      s = str(stone)
+      if len(s) % 2 == 0:
+        next_stones = [int(s[:len(s) // 2]), int(s[len(s) // 2:])]
+      else:
+        next_stones = [stone * 2024]
+    if n == 1:
+      return len(next_stones)
+    else:
+      return sum(blink(stone, n - 1) for stone in next_stones)
+
+
+def part1(stones):
+  return sum(blink(stone, n=25) for stone in stones)
+
+
+def part2(stones):
+  return sum(blink(stone, n=75) for stone in stones)
+
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('-c', '--clip', '--copy', action='store_true',
+                      help='Copy answer to clipboard')
+  parser.add_argument('-p', '--part', type=int, choices=(1, 2),
+                      help='Which part to run (default: both)')
+  parser.add_argument('-1', '--part1', action='store_const', dest='part',
+                      const=1, help='Part 1 only')
+  parser.add_argument('-2', '--part2', action='store_const', dest='part',
+                      const=2, help='Part 2 only')
+  parser.add_argument('input', nargs='?', metavar='input.txt')
+  args = parser.parse_args()
+  if args.clip:
+    import pyperclip
+  input = get_input(args.input)
+  if not args.part or args.part == 1:
+    answer1 = part1(input)
+    print(answer1)
+    if args.clip and answer1 is not None:
+      pyperclip.copy(str(answer1))
+  if not args.part or args.part == 2:
+    answer2 = part2(input)
+    print(answer2)
+    if args.clip and answer2 is not None:
+      pyperclip.copy(str(answer2))
